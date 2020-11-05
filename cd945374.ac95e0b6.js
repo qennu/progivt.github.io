a2cae96ecc3d084a88b1f1112274b9cfdc01ac23
Aleksandr Pavlov <7911562+av-pavlov@users.noreply.github.com>
2020-11-05 19:08:53 +0900

    Deploy website - based on d95859d63646c08a95db5c85091a14eb49aece89

---
+++ cd945374.ac95e0b6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{73:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"\u041c\u0435\u0433\u0430\u0441\u0430\u0439\u0442 \u0432 17 \u043b\u0435\u0442","permalink":"/ivt20/blog/hola"}]}')}}]);
